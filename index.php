5eea0afce46035d487ce2e0ed03f315d573b1424
Riccardo-Finello <74531640+Riccardo-Finello@users.noreply.github.com>
2022-12-03 08:56:18 +0100

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>The Rossi Cinema</h1>
+    <h3>Film In Evidenza</h3>
+    <div id="movies"></div>
+</body>
+</html>
